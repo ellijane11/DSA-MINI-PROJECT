3963ac706f93f97fbc966cf6fa64a8462ef35d69
Prardhana Anil <Prardhanaanil24BCD31@iiitkottayam.ac.in>
2025-10-01 06:29:00 +0530

    Update page.tsx

--- src/app/choose/page.tsx
+++ src/app/choose/page.tsx
@@ -3,14 +3,20 @@ import Link from "next/link";
 
 const ChooseRolePage = () => {
     return (
-        <div className="choose-role-container min-h-screen  bg-[#01003d] text-white flex flex-col items-center justify-center">
+        <div
+            className="min-h-screen text-white flex flex-col items-center justify-center"
+            style={{
+                backgroundImage:
+                    "linear-gradient(to right, var(--btn-grad-start), var(--btn-grad-mid), var(--btn-grad-end))",
+            }}
+        >
             <h1 className="main-title text-4xl font-bold mb-4">Ride It</h1>
             <h2 className="subtitle text-2xl font-semibold mb-8">Choose Your Role</h2>
 
             <div className="roles flex flex-col sm:flex-row gap-6">
                 <Link
                     href="/passenger"
-                    className="role-card bg-blue-100 hover:bg-blue-200 text-blue-900 px-6 py-4 rounded-lg shadow-md text-lg font-medium text-center transition-colors duration-200"
+                    className="role-card bg-blue-100 hover:bg-blue-200 text-blue-900 px-6 py-4 rounded-2xl shadow-md text-lg font-medium text-center transition-colors duration-200"
                 >
                     <span>Are you a passenger 👥</span>
                 </Link>
@@ -27,4 +33,5 @@ const ChooseRolePage = () => {
 };
 
 export default ChooseRolePage;
+
 
